d30623e94635d0103e6cb8effb4a06a31e198bba
Garret Gallant <gallanttechnological@gmail.com>
2022-07-06 13:11:44 -0600

    lesson complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    //target paragraph element with id text and replace w/ This is really cool
+    const elementToMod = document.getElementById("text");
+    elementToMod.textContent = `This is really cool!`
+  })
+
+
+
+
+
